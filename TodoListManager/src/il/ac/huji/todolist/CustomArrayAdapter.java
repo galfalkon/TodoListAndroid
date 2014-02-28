f79c6d955007b60c1c5e2c1f0156e81c0fa54a8d
galfalkon <galfalkon@hotmail.com>
2014-02-28 16:18:38 +0200

    Fix the padding of the listview elements
    Moved the padding from the adapter to the layout definition.

--- TodoListManager/src/il/ac/huji/todolist/CustomArrayAdapter.java
+++ TodoListManager/src/il/ac/huji/todolist/CustomArrayAdapter.java
@@ -18,13 +18,8 @@ public class CustomArrayAdapter extends ArrayAdapter<String> {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		TextView view = (TextView)super.getView(position, convertView, parent);
 		view.setTextColor(ITEMS_COLORS[position % ITEMS_COLORS.length]);
-		view.setPadding(PADDING_LEFT, PADDING_TOP, PADDING_RIGHT, PADDING_BOTTOM);
 		return view;
 	}
 	
 	private static final int[] ITEMS_COLORS = {Color.RED, Color.BLUE};
-	private static final int PADDING_LEFT = 0;
-	private static final int PADDING_TOP = 10;
-	private static final int PADDING_RIGHT = 0;
-	private static final int PADDING_BOTTOM = 10;
 }
